2692da30ad2c2c907d43d96c9c28b05467b32c4c
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2025-01-03 16:55:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
 
+    // Display the first and last name in an alert
+    alert(firstName + ' ' + lastName);
 }
