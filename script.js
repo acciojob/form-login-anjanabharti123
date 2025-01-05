283aea0e4ceb6ce5d19b39e3bcfd0f67364d5308
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2025-01-05 20:01:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-const firstName = document.getElementById('first-name').value;
-    const lastName = document.getElementById('last-name').value;
+event.preventDefault();
 
-    // Display the first and last name in an alert
-    alert(firstName + ' ' + lastName);
+    // Get the values of the first and last name from the form
+    const firstName = document.forms['form1']['fname'].value;
+    const lastName = document.forms['form1']['lname'].value;
+
+    // Alert the full name by concatenating the first and last names
+    alert(firstName + " " + lastName);
 }
